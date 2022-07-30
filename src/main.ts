7e38c7cfdc5eb34191b0fb31c6d44421f8db6555
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2022-07-30 13:16:44 -0300

    swagger added, typeorm added, services added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { environment } from './environment/environment';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle(environment.SWAGGER_TITLE)
+    .setDescription('A API do Post-it')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(environment.PORT || 3000);
 }
 bootstrap();
