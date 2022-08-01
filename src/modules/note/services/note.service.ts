e0fa08a6ea3836da71e9ceae84c4402b65e2384c
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2022-08-01 13:37:29 -0300

    removed subtraction from skip() call

--- src/modules/note/services/note.service.ts
+++ src/modules/note/services/note.service.ts
@@ -33,7 +33,7 @@ export class NoteService {
     .leftJoinAndMapOne('comments.user', 'comments.user', 'commentUsers')
     .leftJoinAndMapMany('note.likes', 'note.likes', 'likes', 'likes.userId = :userId', { userId: requestUser.id })
     .orderBy('note.updatedAt', 'DESC')
-    .skip((page-1)*4)
+    .skip(page*4)
     .take(4)
     .getMany();
   }
