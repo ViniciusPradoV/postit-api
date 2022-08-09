63821f345e8fe35ff5b0c847e0d7e5bdb845ec9c
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2022-08-09 15:59:54 -0300

    feat(comment) removed annotation from create-note-comment-payload

--- src/modules/note-comment/models/create-note-comment.payload.ts
+++ src/modules/note-comment/models/create-note-comment.payload.ts
@@ -10,10 +10,4 @@ import { IsDefined, IsNotEmpty, IsString, Length, MaxLength, MinLength } from "c
     @IsNotEmpty({message: 'O conteúdo do comentário não pode ser vazio'})
     @MaxLength(250, {message: 'O conteúdo do comentário não pode ultrapassar 250 caracteres'})
     public comment: string;
-
-    @ApiProperty()
-    @IsDefined({message: 'O corpo da nota deve ser definido'})
-    @IsString({message: 'O corpo da nota deve ser uma string'})
-    @IsNotEmpty({message: 'O corpo da nota não pode ser vazio'})
-    public annotation: string;
  }
